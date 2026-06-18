e0a46751691b745a0e12d093633412d69ada6e28
napNsleep <245271671+napNsleep@users.noreply.github.com>
2026-06-17 21:43:39 -0500

    modified setup.sh to be a multiple choice script

--- setup.sh
+++ setup.sh
@@ -1,53 +1,22 @@
 #!/bin/bash
 
-FF_CONF="$HOME/.config/mozilla/firefox/*.default-release"
 
-DNF_PACKAGES=(
-	mesa-va-drivers-freeworld 
-	gnome-tweaks 
-	mullvad-vpn 
-	steam 
-	kitty 
-	starship 
-	hydrapaper
-	piper
-	flatseal 
-	neovim 
-	gnome-tweaks 
-	gnome-shell-extension-forge
-	gnome-shell-extension-appindicator
-	dconf-editor
-)
+echo "1. After installation script
+2. Set keyboard shortcuts
+3. Finalize installation"
 
-FLATPAK_PACKAGES=(
-	flathub com.mattjakeman.ExtensionManager 
-	com.discordapp.Discord 
-	org.localsend.localsend_app	
-)
+read -p $'Choose [1-3]: ' choice
 
-# initialize firefox profile
-timeout 5s firefox --headless
+if [[ "$choice" == 1 ]]; then
+	./after_install.sh
 
-# firewall config
-sudo firewall-cmd --set-default-zone=drop
-sudo firewall-cmd --permanent --zone=drop --add-service=ssh \--add-service=dhcpv6-client \--add-service=samba-client
-firewall-cmd --reload
+elif [[ "$choice" == 2 ]]; then
+	./keyboard_shortcuts.sh
 
-# adding repos
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo 
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf copr enable @neurofedora/neurofedora-extra ; sudo dnf copr enable atim/starship
-sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
+elif [[ "$choice" == 3 ]]; then
+	./finalize.sh
 
-# swapping to full ffmpeg
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+fi
 
-# install personal apps
-sudo dnf install "${DNF_PACKAGES[@]}" --skip-broken --skip-unavailable -y
-flatpak install "${FLATPAK_PACKAGES[@]}" -y
 
-# copy files in right directories
-cp ./.bashrc $HOME
-cp -r ./.bin $HOME
-cp -r ./firefox/chrome "$FF_CONF"
-cp -r ./.config/* $HOME/.config
+
